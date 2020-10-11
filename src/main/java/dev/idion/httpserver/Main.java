f3e9c9c2838b93af5471235e3954d2c2c4612da2
IdionKim <ksundong@gmail.com>
2020-10-11 12:22:04 +0900

    feat: 들어온 요청 메시지를 출력하는 프로그램 작성

---
+++ src/main/java/dev/idion/httpserver/Main.java
@@ -0,0 +1,26 @@
+package dev.idion.httpserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+  public static final int DEFAULT_PORT = 80;
+
+  public static void main(String[] args) {
+    try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
+      while (true) {
+        try (Socket clientSocket = serverSocket.accept()) {
+          BufferedReader reader = new BufferedReader(
+              new InputStreamReader(clientSocket.getInputStream()));
+          reader.lines().forEach(System.out::println);
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
