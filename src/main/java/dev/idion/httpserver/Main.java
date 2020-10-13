b7cd4351562d1e0e642f4606da5e0f3b739d90ee
IdionKim <ksundong@gmail.com>
2020-10-13 23:15:13 +0900

    feat: 클라이언트 요청을 핸들링하는 부분 추가

--- src/main/java/dev/idion/httpserver/Main.java
+++ src/main/java/dev/idion/httpserver/Main.java
@@ -5,22 +5,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
   private static final int DEFAULT_PORT = 80;
 
   public static void main(String[] args) {
     try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
       while (true) {
-        try (Socket clientSocket = serverSocket.accept()) {
-          BufferedReader reader = new BufferedReader(
-              new InputStreamReader(clientSocket.getInputStream()));
-          reader.lines().forEach(System.out::println);
+        try (Socket client = serverSocket.accept()) {
+          handleClient(client);
         }
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private static void handleClient(Socket client) throws IOException {
+    log.debug("New Client: {}", client);
+    BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+    StringBuilder requestBuilder = new StringBuilder();
+    String line;
+
+    while (!(line = br.readLine()).isBlank()) {
+      requestBuilder.append(line).append("\r\n");
+    }
+
+    log.debug(requestBuilder.toString());
   }
 }
